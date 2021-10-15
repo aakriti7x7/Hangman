fc0c759b7797fe02eb6988b269684d5f676896c1
l0ginp@gmail.com <l0ginp@gmail.com>
2021-10-15 15:13:21 +0700

    add game level ,text color and text layout

--- hangman.py
+++ hangman.py
@@ -5,6 +5,23 @@
 import random
 from word import word
 import string
+from termcolor import colored
+import inquirer
+
+GAME_LEVEL = [
+  inquirer.List('level',
+                message="Please select game level",
+                choices=['Easiest', 'Easy', 'Normal', 'Hard', 'Nightmare'],
+            ),
+]
+
+GAME_LEVEL_MAPPING = {
+  "Easiest" : 99,
+  "Easy" : 10,
+  "Normal" : 6,
+  "Hard" : 3,
+  "Nightmare" : 1,
+}
 
 def valid(word):
   w = random.choice(word)
@@ -14,17 +31,17 @@ def valid(word):
   return w.upper()
 
 
-def hangman():
+def hangman(game_level):
 
   w = valid(word)
   word_letter =  set(w)
   alphabet = set(string.ascii_uppercase)
   used_letter = set() # empty for now
 
-  lives = 6
+  lives = game_level
   while len(word_letter)>0 and lives>0:
 
-    print("you have", lives,"lives left used: "," " .join(used_letter) )
+    print("\nyou have", lives,"lives left used: "," " .join(used_letter) )
 
     #list for[w o _ d]
     currentCondition=[letter if letter in used_letter else "_" for letter in w]
@@ -40,20 +57,28 @@ def hangman():
       else:
         lives=lives - 1
     elif user_letter in used_letter:
-      print("already used, try smthing else")
+      print(colored("already used, try smthing else", "red"))
       lives = lives - 1
     
     else:
-      print("invalid character")
+      print(colored("invalid character", "red"))
 
   if lives==0:
-    print("u died the word was", w)
+    msg = colored("u died the word was ", "red")
+    word_answer = colored(w, "magenta")
+    print(f"{msg}: {word_answer}")
   else:
-    print("yey u guessed",w)
+    msg = colored("yey u guessed ", "green")
+    word_answer = colored(w, "magenta")
+    print(f"{msg}: {word_answer}")
 
 
+if __name__ == "__main__":
+  # # print("type smthing to start\n")
+  # user_input = input("type something to start then [ENTER]\n>> ")
+  # print(colored(user_input, 'green'))
 
-user_input = input('type smthing to start')
-print(user_input)
+  answers = inquirer.prompt(GAME_LEVEL)
+  game_level = GAME_LEVEL_MAPPING.get(answers["level"])
 
-hangman()
+  hangman(game_level)
